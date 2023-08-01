b1a69d1505fa9cecca19458930761db30e97a6be
diasporx <60825178+diasporx@users.noreply.github.com>
2023-08-01 21:39:30 +0300

    Add Header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Header/>
   <router-view/>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Header from '@/components/__include/Header.vue'
+export default {
+  components: {Header}
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+</script>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.container {
+  display: flex;
+  max-width: calc(100% - 260px);
+  height: 100%;
+  margin: 0 auto;
+}
+.d-flex {
+  display: flex;
+}
+.justify-content-between {
+  justify-content: space-between;
+}
+.align-items-center {
+  align-items: center;
+}
+.w-100 {
+  width: 100%;
 }
 </style>
---
+++ src/components/__include/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <div class="container">
+      <div class="d-flex w-100 justify-content-between">
+        <div class="logo">
+          <router-link to="/"><img alt="logo" src='@/assets/logo.svg'></router-link>
+        </div>
+        <div class="like d-flex align-items-center">
+          <router-link to="/about"><img alt="logo" src='@/assets/like.svg'></router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.header {
+  width: 100%;
+  padding: 11px;
+}
+</style>
+
+<script setup>
+
+</script>
